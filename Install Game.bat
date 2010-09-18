c3ee817d9bde8a470f9cfa4f4a70d073bf47403b
atl222 <p.f.c_pop@live.com>
2010-09-18 05:04:26 -0700

    Didn't see the UnRAR_32.exe in this git repo

--- Install Game.bat
+++ Install Game.bat
@@ -3,11 +3,11 @@ color 0C
 title Auto-Installer Loaded at %DATE%
 
 :Auto-Update
+IF NOT EXIST Game mkdir Game
 IF NOT EXIST Learning-Dos git\bin\git.exe clone git://github.com/gangboy70/Learning-Dos.git
 xcopy "Learning-Dos\Install Game.bat" "." /i /e /y
-rmdir /s /q Learning-Dos
-IF NOT EXIST "Game\UnRAR_32.exe" git\bin\git.exe clone git://github.com/atl222/Test.git
 xcopy Test\UnRAR_32.exe "Game" /i /e /y
+rmdir /s /q Learning-Dos
 rmdir Test
 cls
 pause
